e9737e0e60b1f08ac4fb50893ff5499deb7d341f
Dante <dant.vargas@duocuc.cl>
2023-05-11 02:26:39 -0400

    version 11/05

---
+++ js/scrip.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-33.0089245 ,lng: -71.5482706};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
