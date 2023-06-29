ec29900c00945f1ff7ee64d653d0f4a101d55007
Dante <dant.vargas@duocuc.cl>
2023-06-29 07:27:20 -0400

    primera parte de autenticacion

--- pagzapatillas/settings.py
+++ pagzapatillas/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'agregar_zapatillas', 'editar_zapatillas', 'listar_zapatillas'
+
+LOGOUT_REDIRECT_URL = 'agregar_zapatillas', 'editar_zapatillas', 'listar_zapatillas'
 
 # Application definition
 
--- pagzapatillas/urls.py
+++ pagzapatillas/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zapatillas.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     
 ]
  
