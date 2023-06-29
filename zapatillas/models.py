2e32e0385e930f244d48e7d9632fe2bfef927a07
Dante <dant.vargas@duocuc.cl>
2023-06-29 00:38:01 -0400

    base de datos

--- zapatillas/models.py
+++ zapatillas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre= models.CharField(max_length=50)
+
+
+class Producto(models.Model):
+    id_zapatillas = models.CharField( primary_key=True, max_length=10),
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    color = models.CharField(max_length=50)
+    marca =models.ForeignKey(Marca, on_delete=models.PROTECT)
+    
